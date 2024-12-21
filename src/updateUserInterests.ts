9fa03a3397e5c07240c4991213c47f42b4377480
Vladislav Garashkin <garashkinvlad@gmail.com>
2024-12-22 01:39:50 +0300

    refactor: updateUserInterests.ts

--- src/updateUserInterests.ts
+++ src/updateUserInterests.ts
@@ -1,37 +1,31 @@
-const updateUserHandle = (handle) => {
+// Что было плохо и почему:
+// Дублирование кода. Нарушение принципа DRY.
+// Нет типизации параметров функций, что снижает читаемость и может привести к ошибкам.
+
+// Что стало лучше и почему:
+// Создание общей функции `updateUserData`, которая инкапсулирует общую логику для работы с запросами, уменьшает повторение кода, облегчая поддержку и масштабирование.
+// Добавлена типизация параметров, что повышает безопасность типов и читаемость.
+// Можно использовать одну точку входа для различных типов данных пользователя.
+// Эндпоинты вынесены в enum, для указания типа и для передачи значения при вызове функции, чтобы избежать ошибки при передаче параметра.
+
+enum Endpoints {
+  HANDLE = 'handle',
+  INTERESTS = 'interests',
+}
+
+const updateUserData = <T>(endpoint: Endpoints, data: T): Promise<Response> => {
   if (!isLoggedIn()) {
     toLoginPage();
 
-    return;
+    return Promise.reject('Not logged');
   }
 
   const token = getToken();
   const headers = getHeaders(token);
 
-  return fetch(`${domain}/v1/users/handle`, {
+  return fetch(`${domain}/v1/users/${endpoint}`, {
     method: 'PUT',
     headers,
-    body: JSON.stringify({
-      handle,
-    }),
-  });
-};
-
-const updateUserInterests = (interestUUIDs) => {
-  if (!isLoggedIn()) {
-    toLoginPage();
-
-    return;
-  }
-
-  const token = getToken();
-  const headers = getHeaders(token);
-
-  return fetch(`${domain}/v1/users/interests`, {
-    method: 'PUT',
-    headers,
-    body: JSON.stringify({
-      interestUUIDs,
-    }),
+    body: JSON.stringify(data),
   });
 };
