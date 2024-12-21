0cc2c9d87295f9eff29888a1ffcec67f3c83079f
Vladislav Garashkin <garashkinvlad@gmail.com>
2024-12-21 21:32:47 +0300

    refactor: formatPhoneNumber.ts

--- src/formatPhoneNumber.ts
+++ src/formatPhoneNumber.ts
@@ -1,7 +1,18 @@
-function calculateTotalPrice(quantity, price) {
+// Что было плохо и почему:
+// Отсутствует типизация, что усложняет поддержку кода и приводит к возможным ошибкам.
+// Отсутствует валидация/форматирование в `formatPhoneNumber`.
+
+// Что стало лучше и почему:
+// Добавлены типы, что улучшает читаемость и предотвращает типовые ошибки.
+// В функции formatPhoneNumber добавлено базовое форматирование номера телефона.
+// Что позволяет избежать нежелательный результа
+
+function calculateTotalPrice(quantity: number, price: number): number {
   return quantity * price;
 }
 
-function formatPhoneNumber(number) {
-  return `(${number.slice(0, 3)}) ${number.slice(3, 6)}-${number.slice(6)}`;
+function formatPhoneNumber(number: string): string {
+  const digits = number.replace(/\D/g, '');
+
+  return `(${digits.slice(0, 3)}) ${digits.slice(3, 6)}-${digits.slice(6)}`;
 }
