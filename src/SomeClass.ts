5a75ff920731c91c02fd6e643555fa6737eafd15
Vladislav Garashkin <garashkinvlad@gmail.com>
2024-12-22 00:36:49 +0300

    refactor: SomeClass.ts

--- src/SomeClass.ts
+++ src/SomeClass.ts
@@ -1,22 +1,31 @@
+// Что было плохо и почему:
+// Метод `neverUsedMethod` никогда не вызывается и лишь засоряет код.
+// Условие `if (false)` содержит всегда ложное выражение, из-за чего код внутри никогда не выполняется.
+// Странная конструкция `Exception e` - больше к java подходит, `printStackTrace` - отсутствует в стандартной ошибке,
+// если используется кастомная, то точно стоит указать тип и сделать проверку. Может привести к ошибке.
+
+// Что стало лучше и почему:
+// Удалены неиспользуемые методы и "мертвый код", что улучшило читаемость и снизило сложность.
+// Замена `Exception e`. Указан тип для ошибки и добавлена проверка типа, чтобы избежать ошибок.
+
+class CustomError {
+  printStackTrace() {}
+}
+
 class SomeClass {
-
-  private neverUsedMethod() {
-      console.log("This function is called");
-  }
-
   public someMethod() {
-      if (false) {
-          doSomethingThatNeverHappens();
+    try {
+      this.doSomethingSafe();
+    } catch (e) {
+      if (e instanceof CustomError) {
+        e.printStackTrace();
+      } else {
+        console.error(e);
       }
-
-      try {
-          this.doSomethingSafe();
-      } catch (Exception e) {
-          e.printStackTrace();
-      }
+    }
   }
 
   private doSomethingSafe() {
-     console.log('Safe operation')
+    console.log('Safe operation');
   }
 }
