cdc752c2bee20bf587e688007e5c0dadd2e8b520
Vladislav Garashkin <garashkinvlad@gmail.com>
2024-12-21 21:11:03 +0300

    refactor: fetchData.ts

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,8 +1,18 @@
+// Что было плохо и почему:
+// Использование `.then()` и `.catch()` снижает читаемость.
+// Нет типизации для аргументов и возвращаемого значения.
+
+// Что стало лучше и почему:
+// Использование async/await для улучшения читаемости и взаимодействия с результатом.
+// Добавлена типизация для аргументов и возвращаемых значений.
+
 const DEFAULT_TIMEOUT = 5000;
 
-function fetchData(url: string) {
-  // Используем значение по умолчанию для таймаута
-  fetch(url, { timeout: DEFAULT_TIMEOUT })
-    .then((response) => console.log(response))
-    .catch((error) => console.error(error));
+async function fetchData(url: string): Promise<void> {
+  try {
+    const response: Response = await fetch(url, { timeout: DEFAULT_TIMEOUT });
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
