baad8fdb2fdc43e6aaa7905562ad5e1c664e6f6d
Vladislav Garashkin <garashkinvlad@gmail.com>
2024-12-21 20:58:47 +0300

    refactor: Director.ts

--- src/Director.ts
+++ src/Director.ts
@@ -1,5 +1,20 @@
+// Что было плохо и почему:
+// Нарушение принципа инверсии зависимостей (SOLID).
+// Код жестко связанн с конкретной реализацией и усложняло тестирование и расширение.
+// Также не указаны типы
+
+// Что стало лучше и почему:
+// Добавлен интерфейс `ICalendar`, чтобы `Director` зависел от абстракции, а не от конкретной реализации.
+// Указали типы.
+// Это улучшает гибкость, тестируемость и расширяемость кода.
+
+interface ICalendar {
+  addEvent(event: unknown): void;
+}
+
+
 class Calendar {
-  addEvent(event) {
+  addEvent(event: unknown): void {
     // some logic to add event
   }
 }
@@ -7,11 +22,11 @@ class Calendar {
 class Director {
   calendar: Calendar;
 
-  constructor(calendar: Calendar) {
+  constructor(calendar: ICalendar) {
     this.calendar = calendar;
   }
 
-  scheduleMeeting(event) {
+  scheduleMeeting(event: unknown): void {
     this.calendar.addEvent(event);
   }
 }
